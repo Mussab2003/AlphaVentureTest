eee33be440986115ad873103d1d8effe40200a8b
Mussab2003 <116267339+Mussab2003@users.noreply.github.com>
2025-03-13 01:16:51 +0500

    Solved linting errors for deployment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,4 @@
 import { clsx, type ClassValue } from "clsx"
-import {setCookie, getCookie} from 'cookies-next'
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
